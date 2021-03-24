c05053e9f9dbf08a01a4c7739d29ca94ae7b3fa8
CK <nankeen@users.noreply.github.com>
2021-03-24 19:45:35 +0000

    Add Remote implementation

--- pwn.cc
+++ pwn.cc
@@ -1,18 +1,17 @@
 #include "pwn.h"
 #include <cstdio>
 #include <cstdlib>
-#include <iterator>
-#include <linux/prctl.h>
 #include <string>
-#include <unistd.h>
 #include <iostream>
-#include <sys/types.h>
-#include <signal.h>
-#include <sys/prctl.h>
+#include <sys/socket.h>
 #include <vector>
 #include <functional>
 #include <algorithm>
-#include <sys/ptrace.h>
+
+#include <unistd.h>
+#include <signal.h>
+#include <sys/prctl.h>
+#include <sys/wait.h>
 
 std::string operator*(std::string str, int n)
 {
@@ -34,170 +33,6 @@ namespace pwn {
     std::string p32(uint32_t i)
     {
         return std::string((char *)&i, 4);
-    }
-
-    Process::Process(char *argv[])
-    {
-        // Setup pipes we will use for the child process
-        int stdout_pipe[2], stdin_pipe[2];
-        if (pipe(stdout_pipe) != 0) {
-            perror("failed stdout pipe setup");
-            exit(EXIT_FAILURE);
-        }
-        if (pipe(stdin_pipe) != 0) {
-            perror("failed stdin pipe setup");
-            exit(EXIT_FAILURE);
-        }
-
-        _traced = debug;
-
-        // Fork to spawn child process
-        pid_t pid = fork();
-        if (pid == -1) {
-            perror("failed fork");
-            exit(EXIT_FAILURE);
-        }
-
-        if (pid == 0) {
-            // Child should bind pipes and execute command
-            // Duplicate fd
-            if (dup2(stdout_pipe[1], STDOUT_FILENO) == -1) {
-                perror("failed stdout duplication");
-                exit(EXIT_FAILURE);
-            }
-            if (dup2(stdin_pipe[0], STDIN_FILENO) == -1) {
-                perror("failed stdin duplication");
-                exit(EXIT_FAILURE);
-            }
-
-            if (_traced) {
-                prctl(PR_SET_PTRACER, PR_SET_PTRACER_ANY);
-                // Wait for GDB attach signal this is a hack...
-                char tmp;
-                while(read(stdin_pipe[0], &tmp, 1) > 0 && tmp != '\x69') {}
-            }
-
-            // Close all the useless fds
-            close(stdout_pipe[0]);
-            close(stdout_pipe[1]);
-            close(stdin_pipe[0]);
-            close(stdin_pipe[1]);
-
-            int ret = execve(argv[0], argv, environ);
-            if (ret < 0) {
-                perror(("cannot spawn " + std::string(argv[0])).c_str());
-                exit(EXIT_FAILURE);
-            }
-            exit(ret);
-        }
-
-
-        // Assign to private fields
-        _stdout_fd = stdout_pipe[0];
-        _stdin_fd  = stdin_pipe[1];
-        _pid       = pid;
-        _cmdline   = argv[0];
-
-        // Close unused ends of pipe
-        close(stdout_pipe[1]);
-        close(stdin_pipe[0]);
-    }
-
-    Process::Process(const std::string &exec)
-    {
-        // Setup pipes we will use for the child process
-        int stdout_pipe[2], stdin_pipe[2];
-        if (pipe(stdout_pipe) != 0) {
-            perror("failed stdout pipe setup");
-            exit(EXIT_FAILURE);
-        }
-        if (pipe(stdin_pipe) != 0) {
-            perror("failed stdin pipe setup");
-            exit(EXIT_FAILURE);
-        }
-
-        // Fork to spawn child process
-        pid_t pid = fork();
-        if (pid == -1) {
-            perror("failed fork");
-            exit(EXIT_FAILURE);
-        }
-
-        if (pid == 0) {
-            // Child should bind pipes and execute command
-            // Duplicate fd
-            if (dup2(stdout_pipe[1], STDOUT_FILENO) == -1) {
-                perror("failed stdout duplication");
-                exit(EXIT_FAILURE);
-            }
-            if (dup2(stdin_pipe[0], STDIN_FILENO) == -1) {
-                perror("failed stdin duplication");
-                exit(EXIT_FAILURE);
-            }
-
-            if (_traced) {
-                prctl(PR_SET_PTRACER, PR_SET_PTRACER_ANY);
-                // Wait for GDB attach signal this is a hack...
-                char tmp;
-                while(read(stdin_pipe[0], &tmp, 1) > 0 && tmp != '\x69') {}
-            }
-
-            // Close all the useless fds
-            close(stdout_pipe[0]);
-            close(stdout_pipe[1]);
-            close(stdin_pipe[0]);
-            close(stdin_pipe[1]);
-
-            char *cmd = const_cast<char *>(exec.c_str());
-            char *argv[] = {cmd, NULL};
-            int ret = execve(cmd, argv, environ);
-            if (ret < 0) {
-                perror(("cannot spawn " + exec).c_str());
-                exit(EXIT_FAILURE);
-            }
-            exit(ret);
-        }
-
-        // Assign to private fields
-        _stdout_fd = stdout_pipe[0];
-        _stdin_fd  = stdin_pipe[1];
-        _pid       = pid;
-        _cmdline   = exec;
-
-        // Close unused ends of pipe
-        close(stdout_pipe[1]);
-        close(stdin_pipe[0]);
-    }
-
-    Process::~Process()
-    {
-        close(_stdout_fd);
-        close(_stdin_fd);
-        kill(_pid, SIGKILL);
-    }
-
-    std::string Process::recv(const size_t size)
-    {
-        char *buffer = new char[size];
-        ssize_t n_bytes = read(_stdout_fd, buffer, size);
-        if (n_bytes == -1) {
-            perror(("can't read from PID: " + std::to_string(_pid)).c_str());
-            exit(EXIT_FAILURE);
-        }
-        return std::string(buffer, n_bytes);
-    }
-
-    void Process::send(const std::string &data)
-    {
-        if (write(_stdin_fd, data.c_str(), data.length()) < 0) {
-            perror(("can't write to PID: " + std::to_string(_pid)).c_str());
-            exit(EXIT_FAILURE);
-        }
-    }
-    
-    void Process::sendline(const std::string &data)
-    {
-        send(data + '\n');
     }
 
     std::string cyclic(int n)
@@ -240,6 +75,199 @@ namespace pwn {
             buf.push_back('a' + i);
         }
         return buf + buf.substr(0, n - 1);
+    }
+    
+    void Tube::sendline(const std::string &data)
+    {
+        send(data + '\n');
+    }
+
+    std::string Tube::recvall()
+    {
+        int status;
+        std::string result, tmp;
+        wait_term();
+        do {
+            auto tmp = recv(4096);
+            result += tmp ;
+        } while(tmp != "");
+        return result;
+    }
+
+    Process::Process(char *argv[])
+    {
+        start_process(argv);
+    }
+
+    Process::Process(const std::string &exec)
+    {
+        char *cmd = const_cast<char *>(exec.c_str());
+        char *argv[] = {cmd, NULL};
+        start_process(argv, debug);
+    }
+
+    Process::~Process()
+    {
+        close(_stdout_fd);
+        close(_stdin_fd);
+        kill(_pid, SIGKILL);
+    }
+
+    std::string Process::recv(const size_t size)
+    {
+        char *buffer = new char[size];
+        ssize_t n_bytes = read(_stdout_fd, buffer, size);
+        if (n_bytes == -1) {
+            perror(("can't read from PID: " + std::to_string(_pid)).c_str());
+            exit(EXIT_FAILURE);
+        }
+        auto result = std::string(buffer, n_bytes);
+        delete[] buffer;
+        return result;
+    }
+
+    void Process::send(const std::string &data)
+    {
+        if (write(_stdin_fd, data.c_str(), data.length()) < 0) {
+            perror(("can't write to PID: " + std::to_string(_pid)).c_str());
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    void Process::start_process(char *argv[], const bool debug)
+    {
+        // Setup pipes we will use for the child process
+        int stdout_pipe[2], stdin_pipe[2];
+        if (pipe(stdout_pipe) != 0) {
+            perror("failed stdout pipe setup");
+            exit(EXIT_FAILURE);
+        }
+        if (pipe(stdin_pipe) != 0) {
+            perror("failed stdin pipe setup");
+            exit(EXIT_FAILURE);
+        }
+
+        // Fork to spawn child process
+        pid_t pid = fork();
+        if (pid == -1) {
+            perror("failed fork");
+            exit(EXIT_FAILURE);
+        }
+
+        if (pid == 0) {
+            // Child should bind pipes and execute command
+            // Duplicate fd
+            if (dup2(stdout_pipe[1], STDOUT_FILENO) == -1) {
+                perror("failed stdout duplication");
+                exit(EXIT_FAILURE);
+            }
+            if (dup2(stdin_pipe[0], STDIN_FILENO) == -1) {
+                perror("failed stdin duplication");
+                exit(EXIT_FAILURE);
+            }
+
+            if (debug) {
+                prctl(PR_SET_PTRACER, PR_SET_PTRACER_ANY);
+                // Wait for GDB attach signal this is a hack...
+                char tmp;
+                while(read(stdin_pipe[0], &tmp, 1) > 0 && tmp != '\x69') {}
+            }
+
+            // Close all the useless fds
+            close(stdout_pipe[0]);
+            close(stdout_pipe[1]);
+            close(stdin_pipe[0]);
+            close(stdin_pipe[1]);
+
+            int ret = execve(argv[0], argv, environ);
+            if (ret < 0) {
+                perror(("cannot spawn " + std::string(argv[0])).c_str());
+                exit(EXIT_FAILURE);
+            }
+            exit(ret);
+        }
+
+        // Assign to private fields
+        _stdout_fd = stdout_pipe[0];
+        _stdin_fd  = stdin_pipe[1];
+        _pid       = pid;
+        _cmdline   = argv[0];
+
+        // Close unused ends of pipe
+        close(stdout_pipe[1]);
+        close(stdin_pipe[0]);
+    }
+
+    bool Process::wait_term()
+    {
+        int status;
+        pid_t w = waitpid(_pid, &status, 0);
+        if (w == -1) {
+            perror("wait pid failure");
+            return false;
+        }
+        return true;
+    }
+
+    Remote::Remote(const std::string &host, const int port, int socktype)
+    {
+        _host = host;
+        _port = port;
+        struct addrinfo hints;
+        std::memset(&hints, 0, sizeof(struct addrinfo));
+        hints.ai_family   = AF_UNSPEC;  /* Allow IPv4 or IPv6 */
+        hints.ai_socktype = socktype;
+        hints.ai_flags    = 0;
+        hints.ai_protocol = 0;          /* Any protocol */
+
+        auto port_string = std::to_string(port);
+
+        if (getaddrinfo(host.c_str(), port_string.c_str(), &hints, &_addrinfo) != 0) {
+            perror(("Could not get address info for: " + host + ":" + port_string).c_str());
+            exit(EXIT_FAILURE);
+        }
+
+        if((_sockfd = socket(_addrinfo->ai_family, _addrinfo->ai_socktype, _addrinfo->ai_protocol)) < 0) {
+            perror("socket creation failed");
+            exit(EXIT_FAILURE);
+        }
+        if((_connfd = connect(_sockfd, _addrinfo->ai_addr, _addrinfo->ai_addrlen)) < 0) {
+            perror("connection failed");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    Remote::~Remote()
+    {
+        freeaddrinfo(_addrinfo);
+        close(_sockfd);
+        close(_connfd);
+    }
+
+    void Remote::send(const std::string &data)
+    {
+        if (::send(_sockfd, data.c_str(), data.length(), 0) < 0) {
+            perror("can't send with connection");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    std::string Remote::recv(const size_t size)
+    {
+        char *buffer = new char[size];
+        ssize_t n_bytes = ::recv(_sockfd, buffer, size, 0);
+        if (n_bytes == -1) {
+            perror("can't read from connection");
+            exit(EXIT_FAILURE);
+        }
+        auto result = std::string(buffer, n_bytes);
+        delete[] buffer;
+        return result;
+    }
+
+    bool Remote::wait_term()
+    {
+        return true;
     }
 
     namespace gdb {
--- pwn.h
+++ pwn.h
@@ -1,44 +1,89 @@
 #pragma once
-#include <cstddef>
+#include <cstdlib>
 #include <sched.h>
 #include <string>
+#include <cstring>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
 std::string operator*(std::string str, int n);
 
 namespace pwn {
-    std::string p32(uint32_t i);
-    std::string p64(uint64_t i);
-    std::string cyclic(int n);
-    std::string deBruijn(int k, int n);
-
+    // Context flag to determine if debug mode should be enabled
     extern bool debug;
 
+    /*
+     * Convert a 32bit little endian unsigned integer into a 4 byte string
+     * @param i (uint32_t) integer to pack
+     */
+    std::string p32(uint32_t i);
+    /*
+     * Convert a 64bit little endian unsigned integer into a 8 byte string
+     * @param i (uint64_t) integer to pack
+     */
+    std::string p64(uint64_t i);
+    /*
+     * Generate a deBruijn sequence of length n
+     * @param n (int) length of sequence
+     */
+    std::string cyclic(int n);
+    /*
+     * Generate the full deBruijn sequence of a k-sized alphabet of order n
+     * @param k (int) number of alphabets in the sequence
+     * @param n (int) order of de Bruijn sequence
+     */
+    std::string deBruijn(int k, int n);
+
+    /*
+     * Tube class describes an interface that does IO with
+     * an external interface like stdio/sockets
+     */
     class Tube
     {
     public:
         virtual void send(const std::string &data) = 0;
         virtual std::string recv(const size_t size) = 0;
+        virtual bool wait_term() = 0;
+        void sendline(const std::string &data);
+        std::string recvall();
     };
 
-    class Process: Tube
+    class Process: public Tube
     {
     public:
         Process(const std::string &exec);
         Process(char *argv[]);
         ~Process();
 
-        void send(const std::string &data);
-        void sendline(const std::string &data);
-        std::string recv(const size_t size);
+        void send(const std::string &data) override;
+        std::string recv(const size_t size) override;
         pid_t pid() { return _pid; }
         std::string cmdline() { return _cmdline; }
         bool traced() { return _traced; }
+        bool wait_term() override;
 
     private:
+        void start_process(char *argv[], const bool trace = false);
         int _stdout_fd, _stdin_fd;
         pid_t _pid;
         std::string _cmdline;
         bool _traced;
+    };
+
+    class Remote: public Tube
+    {
+    public:
+        Remote(const std::string &host, const int port, int socktype = SOCK_STREAM);
+        ~Remote();
+
+        void send(const std::string &data) override;
+        std::string recv(const size_t size) override;
+        bool wait_term() override;
+    private:
+        std::string _host;
+        int _port, _sockfd, _connfd;
+        struct addrinfo *_addrinfo;
     };
 
     namespace gdb {
